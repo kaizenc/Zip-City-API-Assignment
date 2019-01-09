6b7d5ad17f70a0822c1d4d94ad3e8feed0d275cb
Liudmila Zyrianova <liudmila.zyrianova@gmail.com>
2019-01-09 10:40:06 -0500

    Add ParticularCity component

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,24 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ParticularCity extends Component {
+
+  render() {
+      var {
+          locationText,
+          lat,
+          long,
+          estimatedPopulation,
+          totalWages,
+      } = this.props.data;
+      return (
+          <li>
+              <p>{locationText}</p>
+              <p>{lat}</p>
+              <p>{long}</p>
+              <p>{estimatedPopulation}</p>
+              <p>{totalWages}</p>
+          </li>
+      );
+  }
+}
+
+class ZipInfo extends Component {
+
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h2>Cryptocurrency Ticker</h2>
+        </div>
+        <Tickers />
       </div>
     );
   }
