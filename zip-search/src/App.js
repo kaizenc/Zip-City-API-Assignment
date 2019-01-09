07e29aeca25f0251a3abd2d1d507060f39456b4d
kaizenc <imgtn159@gmail.com>
2019-01-09 10:47:24 -0500

    API work, dont' know what i'm diong

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+class ZipInfo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [
+            {
+                LocationText: "CityName",
+                State: "StateName",
+                Lat: "0",
+                Long: "0",
+                EstimatedPopulation: "0",
+                TotalWages: "1",
+            }
+        ]
+    };
+  }
+  fetchZipData(zipcode){
+    axios.get("https://ctp-zip-api.herokuapp.com/zip/" + zipcode)
+    .then(response => {
+      var wanted = ["LocationText", "State", "Lat", "Long", "EstimatedPopulation", "TotalWages"];
+      var result = response.data.filter(currency => wanted.includes(currency.id));
+      this.setState({data:result});
+    })
+    .catch(err => console.log(err));
+  }
+
+  render(){
+    this.fetchZipData(10016);
+    return(
+      <p>hello</p>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -20,6 +55,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <ZipInfo />
       </div>
     );
   }
