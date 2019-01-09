f69efd5eedd2411ef411ed767efb5585560c3555
kaizenc <imgtn159@gmail.com>
2019-01-09 11:21:18 -0500

    Added axios and got city API to work

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+class ZipInfo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      zipcodes: ["11102"],
+    };
+  }
+  fetchZipCodes(cityName){
+    axios.get("https://ctp-zip-api.herokuapp.com/city/" + cityName)
+    .then(response => {
+      var result = response.data;
+      this.setState({zipcodes:result});
+      console.log(this.state.zipcodes);
+    })
+    .catch(err => console.log(err));
+  }
+
+  render(){
+    this.fetchZipCodes(this.props.cityName);
+    var zipcodes = this.state.zipcodes.map((zipcode)=>
+      <li>{zipcode}</li>
+    );
+    return(
+      <ul>{zipcodes}</ul>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          City Search
         </header>
+        <ZipInfo cityName="SPRINGFIELD"/>
       </div>
     );
   }
