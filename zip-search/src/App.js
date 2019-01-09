4cf1de7fe975914e0d8e0cd91d714c36d7bf3f45
kaizenc <imgtn159@gmail.com>
2019-01-09 10:49:24 -0500

    modified?

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -67,28 +67,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <div className="App-header">
           <h2>Cryptocurrency Ticker</h2>
         </div>
         <Tickers />
-=======
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <ZipInfo />
->>>>>>> kaizen
       </div>
     );
   }
